6ffd3603072578805ea1537597f561c0436162a8
LP (BB) <lee.prosser@embersoft.co.uk>
2019-01-30 09:08:50 +0000

    Have things building
    Not entirely sure how well it will work, going to try it now.

--- vue.config.js
+++
@@ -1,8 +0,0 @@
-const {configureExternals, configureTsLoader} = require("./webpack.extension");
-
-module.exports = {
-    chainWebpack: chain => { configureTsLoader(chain); },
-    configureWebpack: config => { configureExternals(config); },
-    css: { extract: false },
-    parallel: false
-};
